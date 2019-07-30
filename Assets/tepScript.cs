67c0972f983451f0f8c0627c25fec7b28c91353c
Lokesh <lokeshn@chetu.com>
2019-07-30 23:15:34 +0530

    Kamlesh's  stuff

---
+++ Assets/tepScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class tepScript : MonoBehaviour
+{
+
+    public Button[] all;
+    public Button robot;
+
+    private void Start()
+    {
+
+        robot.onClick = null;
+        BaseEventData baseEvent = new BaseEventData(EventSystem.current);
+        ExecuteEvents.Execute(robot.gameObject, baseEvent, ExecuteEvents.submitHandler);
+
+       // ExecuteEvents.Execute(robot, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
+        // robot.onClick.Invoke();
+        //all = GetComponentsInChildren<Button>();
+        // StartCoroutine(sdjfh(2.5f));
+    }
+
+
+    IEnumerator sdjfh(float time)
+    {
+        for (int i = 0; i < all.Length; i++)
+        {
+            yield return new WaitForSeconds(time);
+            all[i].Select();
+            yield return new WaitForSeconds(time);
+            all[i].onClick.Invoke();
+
+            yield return new WaitForSeconds(time);
+            all[i].interactable = false;
+
+
+        }
+
+    }
+
+}
